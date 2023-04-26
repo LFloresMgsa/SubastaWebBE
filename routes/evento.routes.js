862e907b9002d9fb0729a2d58c003a9a2c37e2ee
LFloresMgsa <107148012+LFloresMgsa@users.noreply.github.com>
2023-04-26 15:34:39 -0500

    .

--- routes/evento.routes.js
+++ routes/evento.routes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post('/vtm_evento/',eventoController.getEventosCab);
 router.post('/vtd_evento/',eventoController.getEventosDet);
 router.post('/vtd_evento_puja/',eventoController.getEventosDetPuja);
-router.put('/vtd_evento_puja/',eventoController.getEventosDetPuja);
+
 
 router.post('/lgd_catalogo_imagenes/',eventoController.getCatalogoImagenes);
 
