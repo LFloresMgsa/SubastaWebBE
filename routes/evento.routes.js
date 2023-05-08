f39eae9b952ee9982b6b63c9c93da1e974cfd908
miguel-ls <miguel.lopez@codesicorp.com>
2023-05-08 12:42:52 -0500

    seguridad en formularios de antenimiento

--- routes/evento.routes.js
+++ routes/evento.routes.js
@@ -8,19 +8,22 @@ const router = express.Router();
 const checkAuth = require("../middleware/checkAuth.js");
 
 // servicios libres
+router.post('/lgm_catalogo_bs/',webController.getCatalogo);
 router.post('/vtm_evento/',webController.getEventosCab);
 router.post('/vtd_evento/',webController.getEventosDet);
 router.post('/vtd_evento_puja/',webController.getEventosDetPuja);
 router.post('/lgd_catalogo_imagenes/',webController.getCatalogoImagenes);
 router.post('/lgm_videoteca/',webController.getVideoteca);
-
-
 router.post('/sgm_usuarios/',webController.getUsuario);
-
 router.post('/auth/',authorization.token);
 
 // servicios con seguridad JWT
-router.post('/lgm_catalogo_bs/',checkAuth,webController.getCatalogo);
+router.post('/lgm_catalogo_bs/auth/',checkAuth,webController.getCatalogo);
+router.post('/vtm_evento/auth/',checkAuth,webController.getEventosCab);
+router.post('/vtd_evento/auth/',checkAuth,webController.getEventosDet);
+router.post('/vtd_evento_puja/auth/',checkAuth,webController.getEventosDetPuja);
+router.post('/lgd_catalogo_imagenes/auth/',checkAuth,webController.getCatalogoImagenes);
+router.post('/lgm_videoteca/auth/',checkAuth,webController.getVideoteca);
 
  
 module.exports = router;
