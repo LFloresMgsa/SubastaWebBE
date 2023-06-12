ade1696180fc3eb057b6336e2d7e4940251cab10
MANAGEMENT GROUP SA <mlopez@mgsa.com.pe>
2023-06-11 21:12:16 -0500

    local

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
+const https = require('https')
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const JWT = require("jsonwebtoken")
+const fs = require('fs')
 
 const catalogsubastaRoutes = require("./routes/evento.routes.js");
 const cors=require("cors");
 
 // settings
 const app = express();
-const PORT = 5000;
+const PORT = 443;
 const corsOptions ={
     origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
@@ -31,7 +33,7 @@ app.use("/api/evento", catalogsubastaRoutes );
 // pagina inicial
 
 app.get("/", function (require, response) {
-    response.send("<h3>Pagina inicial de MYSQL</h3>");
+    response.send("<h3>Galpon Legado API</h3>");
 });
 
 // pagina secundarias
@@ -48,7 +50,17 @@ app.get("/contact", function (require, response) {
     });
 });
 
+//   app.get("/.well-known/pki-validation/7AB190C07F9C6C5FB995E77E1B6B50FB.txt",function(require,response) {
+//     response.sendFile("7AB190C07F9C6C5FB995E77E1B6B50FB.txt",{
+//         root: path.join(__dirname, "./.well-known/pki-validation")
+//     });
+//   });
+
 // init server
-app.listen(PORT, function () {
-    console.log("server running port 5000");
+https.createServer({
+    key: fs.readFileSync('./domain.key'),
+    cert: fs.readFileSync('./certs/galponlegado_com.crt'),
+}, app).listen(PORT, function () {
+    console.log("server running port 443");
 });
+
